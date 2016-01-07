7ef5d716d60941021a8a2798dcabd7d4c549c73c
Thomas Jakeway <tjakeway92@gmail.com>
2016-01-06 19:46:42 -0500

    create a deploymentconfig class to share variables between prod and dev deployment

--- config.py
+++ config.py
@@ -3,27 +3,24 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SPOTIFY_ID = os.environ['SPOTIFY_ID']
     SPOTIFY_SECRET = os.environ['SPOTIFY_SECRET']
     REDIRECT_URI = os.environ['REDIRECT_URI']
 
 
-class ProductionConfig(Config):
+class ProductionConfig(DeploymentConfig):
     DEBUG = False
 
 
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
+class DevelopmentConfig(DeploymentConfig):
     DEVELOPMENT = True
     DEBUG = True
 
