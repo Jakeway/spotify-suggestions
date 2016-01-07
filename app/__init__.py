ca759f653f33b58a52acdf96a18bdd2845011c1e
Thomas Jakeway <tjakeway92@gmail.com>
2016-01-07 16:33:01 -0500

    use redis as a cache

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 import os
+from redis import Redis
+from flask.ext.session import Session
 
 
 app = Flask(__name__)
@@ -11,5 +13,11 @@ db = SQLAlchemy(app)
 Bootstrap(app)
 lm = LoginManager()
 lm.init_app(app)
+redis_host = app.config['REDIS_HOST']
+redis_port = app.config['REDIS_PORT']
+redis_conn = Redis(host=redis_host, port=redis_port)
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_REDIS'] = redis_conn
+Session(app)
 
 from app import views, models
--- config.py
+++ config.py
@@ -5,12 +5,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    SECRET_KEY = os.urandom(24).encode('hex')
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SPOTIFY_ID = os.environ['SPOTIFY_ID']
     SPOTIFY_SECRET = os.environ['SPOTIFY_SECRET']
     REDIRECT_URI = os.environ['REDIRECT_URI']
+    REDIS_HOST = os.environ['REDIS_HOST']
+    REDIS_PORT = os.environ['REDIS_PORT']
 
 
 class TestingConfig(Config):
