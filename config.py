36c161b65e41200aba01e1e33c5e02a94b542baa
Thomas Jakeway <tjakeway92@gmail.com>
2016-01-06 20:47:02 -0500

    added api keys + other secrets to config class

--- config.py
+++ config.py
@@ -7,6 +7,10 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SPOTIFY_ID = os.environ['SPOTIFY_ID']
+    SPOTIFY_SECRET = os.environ['SPOTIFY_SECRET']
+    REDIRECT_URI = os.environ['REDIRECT_URI']
 
 
 class TestingConfig(Config):
@@ -14,17 +18,10 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost/test"
 
 
-class DeploymentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SPOTIFY_ID = os.environ['SPOTIFY_ID']
-    SPOTIFY_SECRET = os.environ['SPOTIFY_SECRET']
-    REDIRECT_URI = os.environ['REDIRECT_URI']
-
-
-class ProductionConfig(DeploymentConfig):
+class ProductionConfig(Config):
     DEBUG = False
 
 
-class DevelopmentConfig(DeploymentConfig):
+class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
